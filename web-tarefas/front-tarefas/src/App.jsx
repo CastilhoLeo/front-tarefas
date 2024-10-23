ba5a5a0c5aeac4734ba2336a0efd54bfe1e1afb0
LeonardoC <leonardo_castilho1@hotmail.com>
2024-10-23 13:27:21 -0300

    Ajustando tabela

--- web-tarefas/front-tarefas/src/App.jsx
+++ web-tarefas/front-tarefas/src/App.jsx
@@ -7,12 +7,17 @@ import TabelaTarefas from './components/TabelaTarefas'
 
 function App() {
 
+  const tarefas = [
+    {id:1, titulo:"Teste1", dataVencimento:"23/10/2024", situacao:"PENDENTE"},
+    {id:2, titulo:"Teste2", dataVencimento:"22/9/2024", situacao:"FINALIZADO"}
+  ]
+
   return (
     
       <div className='App'>
         <Header/>
         <Pesquisa/>
-        <TabelaTarefas/>
+        <TabelaTarefas tarefas={tarefas} />
       </div>
    
     
--- web-tarefas/front-tarefas/src/components/TabelaTarefas.jsx
+++ web-tarefas/front-tarefas/src/components/TabelaTarefas.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import './TabelaTarefas.css'
 
-const TabelaTarefas = ()=>{
+const TabelaTarefas = (props)=>{
     return(
-        <div>
+        <table className='tabelaTarefas'>
             <tr>
                 <th>Id</th>
                 <th>Titulo</th>
                 <th>Data Vencimento</th>
                 <th>Situação</th>
             </tr>
-        </div>
+
+            
+                {props.tarefas.map((t)=>(<tr>
+                    <td>{t.id}</td>
+                    <td>{t.titulo}</td>
+                    <td>{t.dataVencimento}</td>
+                    <td>{t.situacao}</td>
+                    <td>
+                        <button>EDITAR</button>
+                        <button>EXCLUIR</button>
+                        <button>{t.situacao === "PENDENTE"? "FINALIZAR":"REABRIR"}</button> 
+                    </td>
+                </tr>))}
+            
+        </table>
     )
 }
 
